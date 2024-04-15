e4169e78a9ec51bfb4bf9dead50f585e6ffec011
mmdmahdi-niazi <mmniazi7879@gmail.com>
2024-04-15 14:18:16 +0330

    💄 improve component btn

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,24 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      colors : {
-        "primary-yellow" : "#FFB400",
-        "primary-green" : "#7EB942",
-        clr : {
-          "18" : "#18191A",
-          "24" : "#242526",
-          "2b" : "#2B2B2B",
-          "3a" : "#3A3B3C",
-          "76" : "#767676",
-          "e4" : "#E4E6EB",
-          "f0" : "#F0F0F6",
-          "ff" : "#ffffff"
+      colors: {
+        "primary": {
+          DEFAULT: "#FFB400",
         },
+        "green": "#7EB942",
+        clr: {
+          "18": "#18191A",
+          "24": "#242526",
+          "2b": "#2B2B2B",
+          "3a": "#3A3B3C",
+          "76": "#767676",
+          "e4": "#E4E6EB",
+          "f0": "#F0F0F6",
+          "ff": "#ffffff"
+        },
+      },
+      boxShadow:{
+        "box-sm": "0 4px 4px 0 rgba(0, 0, 0, 0.25)"
       }
     },
   },
