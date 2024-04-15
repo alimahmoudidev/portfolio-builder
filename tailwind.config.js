c403e11cd3a8e97a2c0660c068d368d60f816123
mmdmahdi-niazi <mmniazi7879@gmail.com>
2024-04-15 14:03:45 +0330

    🍱 add svg - config tailwind - add component btn

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        "primary-yellow" : "#FFB400",
+        "primary-green" : "#7EB942",
+        clr : {
+          "18" : "#18191A",
+          "24" : "#242526",
+          "2b" : "#2B2B2B",
+          "3a" : "#3A3B3C",
+          "76" : "#767676",
+          "e4" : "#E4E6EB",
+          "f0" : "#F0F0F6",
+          "ff" : "#ffffff"
+        },
+      }
+    },
   },
   plugins: [],
 }
